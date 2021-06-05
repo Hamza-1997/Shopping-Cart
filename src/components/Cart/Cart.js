1d14bfb2a051040a8f254f55b4ea8be7d9ee8435
Hamza Akif <Hamzaakif97@gmail.com>
2021-06-06 03:54:34 +0500

    Added cart to products page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,52 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import '../Cart/Cart.css';
+import ProductList from '../Content/ProductsList';
 function Cart(props) {
+  const [count, setCount] = useState(0);
   return (
-    <div className="parent">
+    <div className="cartWrapper">
       <div
         className={
           props.data ? 'cartParentContainer active' : 'cartParentContainer'
         }
       ></div>
+
       <div className={props.data ? 'cart-container' : 'cart-container close'}>
-        {/* Child  */}
-        <h3 className="closeBtn" onClick={() => props.openCart()}>
-          Close
-        </h3>
-        <div className="cartContent">
-          <h1>Your Shopping Bag</h1>
+        <div className="cartContainerWrapper">
+          {/* Child  */}
+          <button className="closeBtn" onClick={() => props.openCart()}>
+            Close
+          </button>
+          <div className="cartContent">
+            <h1>Your Shopping Bag</h1>
+          </div>
+          <div className="cartListContainer">
+            <img src={ProductList[1].img} alt="" className="cartListImage" />
+            <div className="cartListInfo">
+              <div className="nameAndPrice">
+                <h2>Processor</h2>
+                <span>$48</span>
+              </div>
+              <span className="productName">
+                <h3>AMD ryzen 7 5800X</h3>
+              </span>
+              <div className="quantityController">
+                <button onClick={() => setCount(count - 1)}>-</button>
+                <div className="quantityDisplay">
+                  <input
+                    // data-value
+                    type="text"
+                    className="cartItemQuantityDisplay"
+                    value={count}
+                    onChange={(e) => setCount(Number(e.target.value))}
+                  />
+                </div>
+                <button onClick={() => setCount(count + 1)}>+</button>
+              </div>
+            </div>
+          </div>
+          <div className="cartListSubtotal">Price of Subtotal</div>
+          <button className="checkoutBtn">Checkout</button>
         </div>
       </div>
     </div>
